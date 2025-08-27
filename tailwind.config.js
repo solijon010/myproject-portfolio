5fa3369c916e00bd49a6cc4215d96830c8814454
Sal1kh.Ake <solijonikromov001@gmail.com>
2025-08-27 13:50:40 +0500

    men header va main qismini yarimini responisive xolatda tugatdim

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// // tailwind.config.js
+// module.exports = {
+//   content: ["./*.html", "./**/*.js"], // HTML va JS fayllarni qamrab oladi
+//   theme: {
+//     extend: {
+//       maxWidth: {
+//         'content': '860px', // custom max-width
+//       }
+//     }
+//   },
+//   plugins: [],
+// }
